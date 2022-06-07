4c98d46a0fec72c5bfcccdbc639683da570da786
Rich Hoyt <rhoyt10@gmail.com>
2022-06-06 20:39:58 -0400

    Add collision

--- code/player.py
+++ code/player.py
@@ -3,13 +3,14 @@ from settings import *
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups):
+    def __init__(self, pos, groups, obstacle_sprites):
         super().__init__(groups)
         self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
 
         self.direction = pygame.math.Vector2()
         self.speed = 5
+        self.obstacle_sprites = obstacle_sprites
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -33,7 +34,29 @@ class Player(pygame.sprite.Sprite):
         # slightly faster than left,right,up,down movement
         if self.direction.magnitude() != 0:
             self.direction = self.direction.normalize()
-        self.rect.center += self.direction * speed
+        self.rect.x += self.direction.x * speed
+        self.collision('horizontal')
+        self.rect.y += self.direction.y * speed
+        self.collision('vertical')
+        # self.rect.center += self.direction * speed
+
+    def collision(self, direction):
+        if direction == 'horizontal':
+            for sprite in self.obstacle_sprites:
+                # if the rectangle of sprite collides with rectangle of player
+                if sprite.rect.colliderect(self.rect):
+                    if self.direction.x > 0:  # player moving right
+                        self.rect.right = sprite.rect.left
+                    if self.direction.x < 0:  # player moving left
+                        self.rect.left = sprite.rect.right
+
+        if direction == 'vertical':
+            for sprite in self.obstacle_sprites:
+                if sprite.rect.colliderect(self.rect):
+                    if self.direction.y > 0:  # player moving down
+                        self.rect.bottom = sprite.rect.top
+                    if self.direction.y < 0:  # player moving up
+                        self.rect.top = sprite.rect.bottom
 
     def update(self):
         self.input()
