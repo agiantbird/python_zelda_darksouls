db80d1ba2ac9baf8f7e1d93f859c6c3faa4243c2
Rich Hoyt <rhoyt10@gmail.com>
2022-06-08 20:48:58 -0400

    Add sprite offset

--- code/level.py
+++ code/level.py
@@ -45,11 +45,11 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.offset = pygame.math.Vector2()
 
     def custom_draw(self, player):
-        for sprite in self.sprites():
-            # getting the offset
-            self.offset.x = player.rect.centerx - self.half_width
-            self.offset.y = player.rect.centery - self.half_height
+        # getting the offset
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
 
+        for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset
             self.display_surface.blit(sprite.image, offset_pos)
 
