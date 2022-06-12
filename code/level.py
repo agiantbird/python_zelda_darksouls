b5ec24c08de496e51c8d39a7a16aa785e2b4e8a1
Rich Hoyt <rhoyt10@gmail.com>
2022-06-12 11:35:50 -0400

    Draw random grass graphics

--- code/level.py
+++ code/level.py
@@ -4,6 +4,7 @@ from support import *
 from tile import Tile
 from player import Player
 from debug import debug
+from random import choice
 
 
 class Level:
@@ -20,8 +21,15 @@ class Level:
 
     def create_map(self):
         layouts = {
-            'boundary': import_csv_layout('../map/map_FloorBlocks.csv')
+            'boundary': import_csv_layout('../map/map_FloorBlocks.csv'),
+            'grass': import_csv_layout('../map/map_Grass.csv'),
+            'object': import_csv_layout('../map/map_Objects.csv')
+
         }
+        graphics = {
+            'grass': import_folder('../graphics/Grass')
+        }
+
         for style, layout in layouts.items():
             for row_index, row in enumerate(layout):
                 for col_index, col in enumerate(row):
@@ -30,6 +38,11 @@ class Level:
                         y = row_index * TILESIZE
                         if style == 'boundary':
                             Tile((x, y), [self.obstacle_sprites], 'invisible')
+                        if style == 'grass':
+                            random_grass_image = choice(graphics['grass'])
+                            Tile((x, y), [self.visible_sprites, self.obstacle_sprites], 'grass', random_grass_image)
+                        if style == 'object':
+                            pass
 
         #         if col == 'x':
         #             Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
